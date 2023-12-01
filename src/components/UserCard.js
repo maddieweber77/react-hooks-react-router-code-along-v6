59327bade28fd8fa59c31410f97ee267dcaa3c9d
Maddie Weber <maddieweber7@gmail.com>
2023-12-01 13:57:01 -0500

    done

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,13 @@
-
+// UserCard.js
+import { Link } from "react-router-dom";
 
 function UserCard({user}) {
   return (
     <article>
         <h2>{user.name}</h2>
+        <Link to={`/profile/${user.id}`}>View profile</Link>
     </article>
   );
-};
+}
 
 export default UserCard;
